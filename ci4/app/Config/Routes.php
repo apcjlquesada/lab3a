38b4ff9340596d734381ef673c6be32079fe5e56
Joe Gene Quesada <jlquesada@apc.edu.ph>
2024-06-13 10:23:34 +0800

    CI4 REST API tutorial part 1 GET articles

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -7,11 +7,14 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 
-use App\Controllers\News; // Add this line
+use App\Controllers\Articles; // Add this line
+use App\Controllers\News; 
 use App\Controllers\Pages;
 
-$routes->get('news', [News::class, 'index']);           // Add this line
-$routes->get('news/(:segment)', [News::class, 'show']); // Add this line
+$routes->resource('articles');  // Add this line
+
+$routes->get('news', [News::class, 'index']);           
+$routes->get('news/(:segment)', [News::class, 'show']); 
 
 $routes->get('pages', [Pages::class, 'index']);
 $routes->get('(:segment)', [Pages::class, 'view']);
